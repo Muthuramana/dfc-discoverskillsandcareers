26752c5fe3cf81b53d1c5c90941e6858eb3ff70e
Ben <ben@benchilds.uk>
2019-02-20 15:34:00 +0000

    Question engine looking pretty slick. Content in JSON. Single view.

--- app/routes.js
+++ app/routes.js
@@ -22,4 +22,52 @@ router.get("/", function (req, res) {
 
 })
 
+router.get("/a", function (req, res) {
+
+  res.redirect("launch");
+
+})
+
+// One question route to rule them all!
+router.post(/([a])\/(assessment-q[0-9]*)/, function (req, res) {
+
+  var qUrl = req.params[1];
+  var qNum = Number(qUrl.substr(12));
+
+  // Load the JSON data if not already in session
+  if (!req.session.data['assessment-data']) {
+    var fs = require("fs");
+    var assessmentFile = fs.readFileSync("app/data/assessment.json");
+    // Save JSON in session for use/manipulation
+    req.session.data['assessment-data'] = JSON.parse(assessmentFile);
+  }
+
+  // Setup the assessment data
+  var assessmentData = req.session.data['assessment-data'];
+
+  if (req.session.data['start-assessment']) {
+
+    delete req.session.data['start-assessment'];
+    req.session.data['question-num'] = qNum;
+
+  } else if (!req.session.data['answer']) {
+
+    req.session.data['error-missing'] = true;
+    req.session.data['question-num'] = qNum;
+
+  } else {
+
+    // Unset the current answer/state
+    delete req.session.data['answer'];
+    delete req.session.data['error-missing'];
+
+    // Increment the question
+    req.session.data['question-num'] = qNum + 1;
+
+  }
+
+  res.redirect('assessment-q');
+
+})
+
 module.exports = router
